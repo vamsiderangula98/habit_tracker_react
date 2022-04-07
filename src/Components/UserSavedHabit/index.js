c9f7c70d28fafe48addd7d5e8ff03fb993e98b62
VAMSI KRISHNA DERANGULA <41372768+vamsikrishna987@users.noreply.github.com>
2022-04-07 10:19:06 +0530

    adding user related components

---
+++ src/Components/UserSavedHabit/index.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import WeekdayBox from "../../GenericComponents/WeekdayBox";
+import trashCanIcon from "../../assets/trashcan-icon.svg"
+
+export default function UserSavedHabit(Props) {
+  
+  return (
+    Props.isLoading?"Loading..."
+  :
+    <UserHabitBox>
+      <h1 className="habit-name">{Props.data.name}</h1>
+      <div className="week-list">
+        {Array.from(Array(7).keys()).map((el) => <WeekdayBox key={el} state={Props.data} createMode={false} weekday={el} />)}
+      </div>
+      <img onClick={() => Props.deleteFunction(Props.data.id)} className="delete-icon" src={trashCanIcon} alt="delete habit" />
+    </UserHabitBox>
+  )
+
+}
+
+const UserHabitBox = styled.div`
+
+width: 370px;
+height: 97px;
+
+background:#1d4054;
+border-radius:1rem 1rem;
+padding: 13px;
+margin-top: 10px;
+
+position: relative;
+
+h1{
+font-style: normal;
+font-weight: normal;
+font-size: 19.976px;
+line-height: 25px;
+
+}
+.habit-name{
+  color:lightgreen;
+}
+
+.delete-icon{
+  position: absolute;
+  top: 11px;
+  right: 11px;
+  background-color:wheat;
+
+
+}
+.week-list{
+  padding-top: 8px;
+  display: flex;
+  gap: 4px;
+}
+
+`
